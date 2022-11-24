c73b2529c35202bfe6452f3bcd83bbaa907fdf0c
Alex Singh <115970252+singhalex@users.noreply.github.com>
2022-11-24 10:09:30 -0800

    Add basic match functions

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
+
+console.log(operate(1, 2, "+"));
+console.log(operate(5, 2, "-"));
+console.log(operate(5, 6, "*"));
+console.log(operate(50, 10, "/"));
